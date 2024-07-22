cfdaf6f0994274013b23f159ad0f08f777934d5d
Maliha Shahzadi <maliha.wiva@gmail.com>
2024-07-22 12:06:15 +0500

    Added app with all the integration

---
+++ KyaPakayen/KyaPakayen/KyaPakayenApp.swift
@@ -0,0 +1,20 @@
+//
+//  KyaPakayenApp.swift
+//  KyaPakayen
+//
+//  Created by Maliha on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KyaPakayenApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
