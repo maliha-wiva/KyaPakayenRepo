f235240ba576aa185f511deb2e49a93dd8bafa74
Maliha Shahzadi <maliha.wiva@gmail.com>
2024-07-22 14:05:39 +0500

    Added integration settings

---
+++ KyaPakayen/KyaPakayen/GeneralStartView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralStartView.swift
+//  KyaPakayen
+//
+//  Created by Maliha on 22.07.2024.
+//
+
+import SwiftUI
+
+struct GeneralStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralStartView()
+}
